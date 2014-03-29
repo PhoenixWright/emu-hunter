9a20557289cd99217c6c7075138071e60adf59e4
Stephen Cook <cookincode@gmail.com>
2014-03-29 14:50:21 -0700

    Have weapon, will fire.

---
+++ emuhunter/Assets/Scripts/Weapons/Shooting.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+	public Rigidbody projectile;
+	public float speed = 80;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			Rigidbody instantiatedProjectile = Instantiate(projectile,
+			                                               transform.position,
+			                                               transform.rotation) as Rigidbody;
+
+			instantiatedProjectile.velocity = transform.TransformDirection(
+				new Vector3(0, 0, speed));
+
+			Destroy(instantiatedProjectile.gameObject, 3F);
+		}
+	
+	}
+}
