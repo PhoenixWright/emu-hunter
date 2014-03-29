f3b9a1fb50c1700fc1d2f467767a5436f491cbf3
Christopher Horenstein <chris.horenstein@gmail.com>
2014-03-29 14:04:43 -0700

    Added game modes.

--- emuhunter/Assets/Scripts/GameState.cs
+++ emuhunter/Assets/Scripts/GameState.cs
@@ -1,10 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum GameModes {
+	Normal,
+	BloodRage,
+}
+
 public class GameState : MonoBehaviour
 {
 	public int health = 0; // hits left
 	public int emusDestroyed = 0;
+
+	public GameModes gameMode = GameModes.Normal;
 
 	// Use this for initialization
 	void Start ()
