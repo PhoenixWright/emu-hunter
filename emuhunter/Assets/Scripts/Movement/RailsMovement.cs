f868cbedb2a0bf57895b162da6cf3dc4d27a52c0
Benjamin Adamson <adamson.benjamin@gmail.com>
2014-03-29 14:03:02 -0700

    squash!

--- emuhunter/Assets/Scripts/Movement/RailsMovement.cs
+++ emuhunter/Assets/Scripts/Movement/RailsMovement.cs
@@ -6,7 +6,7 @@ public class RailsMovement : MonoBehaviour {
 	private Vector3 movement;
 
 	// public members
-	public float speed = 6.0F;
+	public float speed = 4.01F;
 
 	// Use this for initialization
 	void Start () {
@@ -14,8 +14,13 @@ public class RailsMovement : MonoBehaviour {
 	}
 
 	Vector3 GetDirectionToMove(float xAxis, float yAxis) {
+		if (!controller.isGrounded) {
+			return Vector3.zero;
+		}
 		//Vector3 direction = new Vector3(xAxis, 0, yAxis);
 		Vector3 direction = this.transform.forward;
+		direction.x += xAxis;
+		direction.y += yAxis;
 		Debug.Log ("basic '" + direction + "'");
 		direction = this.transform.TransformDirection (direction);
 		Debug.Log ("transformDirection '" + direction + "'");
@@ -23,7 +28,7 @@ public class RailsMovement : MonoBehaviour {
 		//Multiply it by speed.
 		direction *= speed;
 		Debug.Log ("with '" + direction + "'");
-		return direction;
+		return direction * Time.deltaTime;
 	}
 
 	// Update is called once per frame
@@ -32,6 +37,7 @@ public class RailsMovement : MonoBehaviour {
 		float yCoord = Input.GetAxis ("Vertical");
 
 		Vector3 direction = GetDirectionToMove (xCoord, yCoord);
-		this.controller.Move (direction);// * Time.deltaTime);
+		transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+		this.controller.Move (direction);
 	}
 }
