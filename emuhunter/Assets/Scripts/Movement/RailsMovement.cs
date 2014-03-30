21552a5403997787742005f20331bf6288c39114
Benjamin Adamson <adamson.benjamin@gmail.com>
2014-03-29 17:46:35 -0700

    Rails movement works!

--- emuhunter/Assets/Scripts/Movement/RailsMovement.cs
+++ emuhunter/Assets/Scripts/Movement/RailsMovement.cs
@@ -1,36 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RailsMovement : MonoBehaviour {
-	private CharacterController playerController;
-	private Vector3 moveDirection;
-	private float horizontalAxis;
-	private float verticalAxis;
+	private Queue<Vector3> positions = new Queue<Vector3>();
+	private CharacterController controller;
+	private Vector3 next;
 
 	// public members
-	public float speed = 6.0F;
+	public float speed = 1.01F;
 
 	// Use this for initialization
 	void Start () {
-		this.playerController = GetComponent<CharacterController>();
-		this.moveDirection = Vector3.one;
-		//this.horizontalAxis = Input.GetAxis ("Horizontal");
-		//this.verticalAxis = Input.GetAxis ("Vertical");
+		this.controller = GetComponent<CharacterController>();
+
+		positions.Enqueue (new Vector3(1.0f, 1.0f, 0.0f));
+		positions.Enqueue (new Vector3(0.0f, 1.0f, 10.0f));
+		positions.Enqueue (new Vector3(10.0f, 1.0f, 10.0f));
+		positions.Enqueue (new Vector3(0.0f, 1.0f, 0.0f));
+		next = positions.Dequeue ();
+	}
+
+	Vector3 GetRotationForCamera() {
+		float xCoord = Input.GetAxis ("Horizontal");
+		float zCoord = Input.GetAxis ("Vertical");
+		return new Vector3 (xCoord, 0.0F, zCoord);
+	}
+
+	Vector3 GetMovement(Transform transform, Vector3 nextPosition) {
+		Vector3 directionToMove = nextPosition - transform.position;
+		Vector3 normalizedDirection = directionToMove.normalized;
+		Vector3 movement = normalizedDirection * speed * Time.deltaTime;
+		return movement;
+	}
+
+	// THIS will be the fn I call of Johns
+	Vector3 GetNextPosition(Transform transform) {
+		float difference = (transform.position - next).magnitude;
+		Debug.Log ("difference '" + difference + "'");
+		if (difference < 1.0F && positions.Count > 0) {
+			Debug.LogWarning ("next");
+			next = positions.Dequeue();
+		} else {
+			Debug.LogWarning ("same");
+		}
+		return next;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		this.horizontalAxis = Input.GetAxis ("Horizontal");
-		this.verticalAxis = Input.GetAxis ("Vertical");
-
-		Vector3 direction = new Vector3(this.horizontalAxis, 0, this.verticalAxis);
-		this.moveDirection = this.transform.TransformDirection (direction);
-		Debug.LogWarning (this.moveDirection);
-
-		//Multiply it by speed.
-		this.moveDirection *= speed;
-		this.playerController.Move (moveDirection);// * Time.deltaTime);
-
-		Debug.Log (this.moveDirection.x + " " + this.moveDirection.y + " " + this.moveDirection.z);
+		Vector3 nextPosition = GetNextPosition(this.controller.transform);
+		Vector3 movement = GetMovement (this.controller.transform, nextPosition);
+		Debug.Log ("pos '" + this.controller.transform.position + "'");
+		this.controller.Move (movement);
 	}
 }
