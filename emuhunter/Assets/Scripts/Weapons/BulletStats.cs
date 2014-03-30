1d70a274e7686b84a4ffcf3c82e86462693fcfd1
Christopher Horenstein <chris.horenstein@gmail.com>
2014-03-29 17:14:52 -0700

    Added bullet stats to bullet.

--- emuhunter/Assets/Scripts/Weapons/BulletStats.cs
+++ emuhunter/Assets/Scripts/Weapons/BulletStats.cs
@@ -1,19 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
-
-	public int health;
+public class BulletStats : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}
-
-	void OnCollisionEnter(Collision collision) {
 	}
 }
