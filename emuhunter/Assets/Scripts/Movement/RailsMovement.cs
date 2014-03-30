5ccf65b162d76681dd63c1fe480c11d1cb6f63a6
Benjamin Adamson <adamson.benjamin@gmail.com>
2014-03-29 17:40:24 -0700

    squash!

--- emuhunter/Assets/Scripts/Movement/RailsMovement.cs
+++ emuhunter/Assets/Scripts/Movement/RailsMovement.cs
@@ -1,48 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RailsMovement : MonoBehaviour {
+	private Queue<Vector3> positions = new Queue<Vector3>();
 	private CharacterController controller;
-	private Vector3 movement;
+	private Vector3 next;
 
 	// public members
-	public float speed = 4.01F;
+	public float speed = 1.01F;
 
 	// Use this for initialization
 	void Start () {
 		this.controller = GetComponent<CharacterController>();
+
+		positions.Enqueue (new Vector3(1.0f, 1.0f, 0.0f));
+		positions.Enqueue (new Vector3(0.0f, 1.0f, 10.0f));
+		positions.Enqueue (new Vector3(10.0f, 1.0f, 10.0f));
+		positions.Enqueue (new Vector3(0.0f, 1.0f, 0.0f));
+		next = positions.Dequeue ();
 	}
 
-	Vector3 GetDirectionToMove(float xAxis, float yAxis) {
-		if (!controller.isGrounded) {
-			return Vector3.zero;
+	Vector3 GetRotationForCamera() {
+		float xCoord = Input.GetAxis ("Horizontal");
+		float zCoord = Input.GetAxis ("Vertical");
+		return new Vector3 (xCoord, 0.0F, zCoord);
+	}
+
+	Vector3 GetMovement(Transform transform, Vector3 nextPosition) {
+		Vector3 directionToMove = nextPosition - transform.position;
+		Vector3 normalizedDirection = directionToMove.normalized;
+		Vector3 movement = normalizedDirection * speed * Time.deltaTime;
+		return movement;
+	}
+
+	// THIS will be the fn I call of Johns
+	Vector3 GetNextPosition(Transform transform) {
+		float difference = (transform.position - next).magnitude;
+		Debug.Log ("difference '" + difference + "'");
+		if (difference < 1.0F && positions.Count > 0) {
+			Debug.LogWarning ("next");
+			next = positions.Dequeue();
+		} else {
+			Debug.LogWarning ("same");
 		}
-		//Vector3 direction = new Vector3(xAxis, 0, yAxis);
-		Vector3 direction = this.transform.forward;
-		direction.x += xAxis;
-		direction.y += yAxis;
-		Debug.Log ("basic '" + direction + "'");
-		direction = this.transform.TransformDirection (direction);
-		Debug.Log ("transformDirection '" + direction + "'");
-
-		//Multiply it by speed.
-		direction *= speed;
-		Debug.Log ("with '" + direction + "'");
-		return direction * Time.deltaTime;
-
-		Vector3 targetDirection = Camera.main.transform.forward;
-		targetDirection.y = 0.0f;
-		targetDirection.Normalize();
-		Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+		return next;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		float xCoord = Input.GetAxis ("Horizontal");
-		float yCoord = Input.GetAxis ("Vertical");
-
-		Vector3 direction = GetDirectionToMove (xCoord, yCoord);
-		transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
-		this.controller.Move (direction);
+		Vector3 nextPosition = GetNextPosition(this.controller.transform);
+		Vector3 movement = GetMovement (this.controller.transform, nextPosition);
+		Debug.Log ("pos '" + this.controller.transform.position + "'");
+		this.controller.Move (movement);
 	}
 }
