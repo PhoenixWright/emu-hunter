5fbda7ca90ef276f1ab993a0fe4ca6e317f3f974
John Pemberton <john.c.pemberton@gmail.com>
2014-03-29 22:57:08 -0700

    Oops, ...

--- emuhunter/Assets/NextCorridor.cs
+++ emuhunter/Assets/NextCorridor.cs
@@ -3,11 +3,32 @@ using System.Collections;
 
 public class NextCorridor : MonoBehaviour {
 
+	private bool _cornerTriggered;
+	private GenerateEnvironment _generateEnvironment;
+	private RailsMovement _railsMovement;
+
 	// Use this for initialization
 	void Start () {
-	
+		_cornerTriggered = false;
+		var scripts = GameObject.FindGameObjectWithTag("GlobalScripts");
+		if (scripts) {
+			_generateEnvironment = scripts.GetComponent<GenerateEnvironment>();
+			_railsMovement = scripts.GetComponent<RailsMovement>();
+		}
 	}
-	
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.GetComponent<PlayerStats> ()) {
+			_cornerTriggered = true;
+			if (_generateEnvironment) {
+				var nextWayPoint = _generateEnvironment.Next();
+				if (_railsMovement) {
+					_railsMovement.AddWaypoint(nextWayPoint);
+				}
+			}
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
