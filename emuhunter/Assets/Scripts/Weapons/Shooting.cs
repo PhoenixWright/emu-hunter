47eb2c71fe339f1bd04fb8f3c7bcf3d6eb1c3261
Benjamin Adamson <adamson.benjamin@gmail.com>
2014-03-29 23:13:07 -0700

    Shoosting works

--- emuhunter/Assets/Scripts/Weapons/Shooting.cs
+++ emuhunter/Assets/Scripts/Weapons/Shooting.cs
@@ -10,20 +10,29 @@ public class Shooting : MonoBehaviour {
 	void Start () {
 	
 	}
+
+	void Attack() {
+		Rigidbody instantiatedProjectile = Instantiate(projectile,
+		                                               transform.position,
+		                                               transform.rotation) as Rigidbody;
+		instantiatedProjectile.velocity = transform.TransformDirection(
+			new Vector3(0, 0, speed));
+
+		//GameObject lightGameObject = new GameObject("The Light");
+		Light lightGameObject = instantiatedProjectile.gameObject.AddComponent<Light> ();
+		//lightGameObject.AddComponent<Light>();
+		lightGameObject.light.color = Color.red;
+		lightGameObject.light.intensity = 20;
+		lightGameObject.light.transform.position = instantiatedProjectile.position;
+
+		Destroy(instantiatedProjectile.gameObject, 3F);
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) 
-		{
-			Rigidbody instantiatedProjectile = Instantiate(projectile,
-			                                               transform.position,
-			                                               transform.rotation) as Rigidbody;
 
-			instantiatedProjectile.velocity = transform.TransformDirection(
-				new Vector3(0, 0, speed));
-
-			Destroy(instantiatedProjectile.gameObject, 3F);
+		if (Input.GetButtonDown ("Fire1")) {
+			Attack();
 		}
-	
 	}
 }
