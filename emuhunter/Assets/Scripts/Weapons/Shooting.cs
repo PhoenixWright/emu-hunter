0675b7111a4bec7d062319f6dda8fcf4550e092b
Benjamin Adamson <adamson.benjamin@gmail.com>
2014-03-29 23:19:50 -0700

    orange less bright light

--- emuhunter/Assets/Scripts/Weapons/Shooting.cs
+++ emuhunter/Assets/Scripts/Weapons/Shooting.cs
@@ -18,11 +18,9 @@ public class Shooting : MonoBehaviour {
 		instantiatedProjectile.velocity = transform.TransformDirection(
 			new Vector3(0, 0, speed));
 
-		//GameObject lightGameObject = new GameObject("The Light");
 		Light lightGameObject = instantiatedProjectile.gameObject.AddComponent<Light> ();
-		//lightGameObject.AddComponent<Light>();
-		lightGameObject.light.color = Color.red;
-		lightGameObject.light.intensity = 20;
+		lightGameObject.light.color = Color.red + Color.yellow;
+		lightGameObject.light.intensity = 10;
 		lightGameObject.light.transform.position = instantiatedProjectile.position;
 
 		Destroy(instantiatedProjectile.gameObject, 3F);
