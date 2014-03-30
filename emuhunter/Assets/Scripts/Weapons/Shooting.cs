99d36b312452002d148bd2e7f4a1ed4af6097a6c
Stephen Cook <cookincode@gmail.com>
2014-03-30 00:28:25 -0700

    Weapons are now detached from player and can be equipped

--- emuhunter/Assets/Scripts/Weapons/Shooting.cs
+++ emuhunter/Assets/Scripts/Weapons/Shooting.cs
@@ -1,29 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Shooting : MonoBehaviour {
+public class Shooting : MonoBehaviour, Weapon {
 	public Rigidbody projectile;
 	public float speed = 80;
 
+	private Transform ammoExit;
 
 	// Use this for initialization
 	void Start () {
-	
+		ammoExit = transform.Find ("Barrel").FindChild ("AmmoExit");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) 
-		{
-			Rigidbody instantiatedProjectile = Instantiate(projectile,
-			                                               transform.position,
-			                                               transform.rotation) as Rigidbody;
 
-			instantiatedProjectile.velocity = transform.TransformDirection(
-				new Vector3(0, 0, speed));
+	}
 
-			Destroy(instantiatedProjectile.gameObject, 3F);
-		}
-	
+	public void Attack () {
+		Rigidbody instantiatedProjectile = Instantiate(projectile,
+		                                               ammoExit.position,
+		                                               ammoExit.rotation) as Rigidbody;
+
+		instantiatedProjectile.velocity = transform.TransformDirection(
+			new Vector3(0, 0, speed));
+
+		Destroy(instantiatedProjectile.gameObject, 3F);
+
 	}
 }
