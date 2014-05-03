af95b8021689fa1cb25c008bfb1ecc7826e8e404
Benjamin Adamson <adamson.benjamin@gmail.com>
2014-05-03 14:05:22 -0700

    Moved Emu construction to builder.

--- emuhunter/Assets/Scripts/Enemies/EmuBehavior.cs
+++ emuhunter/Assets/Scripts/Enemies/EmuBehavior.cs
@@ -2,20 +2,7 @@
 using System.Collections;
 
 public class EmuBehavior : MonoBehaviour {
-	private Emu emu;
-
-	public int health;
-	public int attack; // damage done by emu
-
-	// Use this for initialization
-	void Start ()
-	{
-		var obj = GameObject.FindGameObjectWithTag("GlobalScripts");
-		GameState gameState = obj.GetComponent<GameState>();
-
-		BloodSplatter bloodSplatter = GetComponent<BloodSplatter>();
-		emu = new Emu (this, bloodSplatter, gameState, health, attack);
-	}
+	public Emu emu { private get; set; }
 	
 	// Update is called once per frame
 	void Update ()
