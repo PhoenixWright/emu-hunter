bac4a658afef18d42bf60897c78b828713330ae0
Christopher Horenstein <chris.horenstein@gmail.com>
2014-05-03 16:26:04 -0700

    FREAKING GREEN PLASMA ROCKETS

---
+++ emuhunter/Assets/Scripts/Weapons/RocketGun.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RocketGun : Weapon {
+	/// constants
+	public Vector3 velocityVector = new Vector3(0.0F, 0.0F, 100.0F);
+	private float bulletLifeTime = 3F;
+	private float lightIntensity = 10.0F;
+	private Color lightColor = Color.red + Color.yellow;
+	
+	private SpriteRenderer sprite;
+	
+	// Use this for initialization
+	void Start () {
+		textures = new List<Texture>();
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GA0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GB0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GC0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GD0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GE0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GF0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GG0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GH0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GI0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GJ0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GK0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GL0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GM0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GN0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("RocketGun/BG2GO0", typeof(Texture)));
+		texture = textures[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1") && Time.timeScale > 0) {
+			Debug.Log("Rocket Gun Firing");
+			Attack();
+		}
+	}
+	
+	void OnGUI() {
+		Rect rect = new Rect(((Screen.width / 2) - (texture.width * 2)),
+		                     (Screen.height - (texture.height * 2)),
+		                     texture.width * 4,
+		                     texture.height * 2);
+		GUI.DrawTexture(rect, texture);
+	}
+	
+	override public void Attack() {
+		GameObject bullet = (GameObject)Instantiate(Resources.Load("Bullet"));
+		var forward = Camera.main.transform.TransformDirection(Vector3.forward);
+		var front = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 1.0f));
+		bullet.rigidbody.position = front;
+		bullet.rigidbody.velocity = Camera.main.transform.TransformDirection(velocityVector);
+		Debug.Log(bullet.rigidbody.velocity);
+		
+		Light lightGameObject = bullet.gameObject.AddComponent<Light> ();
+		lightGameObject.light.color = this.lightColor;
+		lightGameObject.light.intensity = this.lightIntensity;
+		
+		Destroy(bullet.gameObject, bulletLifeTime);
+		
+		StartCoroutine(PlayAnimation());
+	}
+	
+}
