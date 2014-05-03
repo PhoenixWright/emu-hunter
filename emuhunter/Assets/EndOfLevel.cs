58b0038739bde70ac48d965880fdc5affac915fe
John Pemberton <john.c.pemberton@gmail.com>
2014-05-03 15:52:58 -0700

    Added end of level stuff

---
+++ emuhunter/Assets/EndOfLevel.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndOfLevel : MonoBehaviour {
+
+	private bool _triggered;
+	private GenerateEnvironment _generateEnvironment;
+	private RailsMovement _railsMovement;
+	
+	// Use this for initialization
+	void Start () {
+		_triggered = false;
+		var scripts = GameObject.FindGameObjectWithTag("GlobalScripts");
+		if (scripts) {
+			_generateEnvironment = scripts.GetComponent<GenerateEnvironment>();
+		}
+		scripts = GameObject.FindGameObjectWithTag("Player");
+		if (scripts) {
+			_railsMovement = scripts.GetComponent<RailsMovement>();
+		}
+	}
+	
+	void OnTriggerEnter(Collider collision) {
+		if (!_triggered && collision.gameObject.GetComponent<PlayerStats> ()) {
+			_triggered = true;
+			if (_generateEnvironment) {
+				// TODO end of level
+				Debug.Log ("Reached end of level");
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
