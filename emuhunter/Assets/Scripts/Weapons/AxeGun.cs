7bdabce46f1a420d86cdf02dbae9c4814766cc7a
Christopher Horenstein <chris.horenstein@gmail.com>
2014-05-03 16:00:29 -0700

    AXE BULLETS

--- emuhunter/Assets/Scripts/Weapons/AxeGun.cs
+++ emuhunter/Assets/Scripts/Weapons/AxeGun.cs
@@ -2,44 +2,50 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class NormalGun : Weapon {
+public class AxeGun : Weapon {
 	/// constants
 	public Vector3 velocityVector = new Vector3(0.0F, 0.0F, 100.0F);
 	private float bulletLifeTime = 3F;
 	private float lightIntensity = 10.0F;
 	private Color lightColor = Color.red + Color.yellow;
-
+	
 	private SpriteRenderer sprite;
+	
+	
 	
 	// Use this for initialization
 	void Start () {
 		textures = new List<Texture>();
-		textures.Add((Texture)Resources.Load("NormalGun/SPRPA0", typeof(Texture)));
-		textures.Add((Texture)Resources.Load("NormalGun/SPRPB0", typeof(Texture)));
-		textures.Add((Texture)Resources.Load("NormalGun/SPRPC0", typeof(Texture)));
-		textures.Add((Texture)Resources.Load("NormalGun/SPRPD0", typeof(Texture)));
-		textures.Add((Texture)Resources.Load("NormalGun/SPRPE0", typeof(Texture)));
-		textures.Add((Texture)Resources.Load("NormalGun/SPRPF0", typeof(Texture)));
-		textures.Add((Texture)Resources.Load("NormalGun/SPRPG0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("AxeGun/AXEGA0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("AxeGun/AXEGB0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("AxeGun/AXEGC0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("AxeGun/AXEGD0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("AxeGun/AXEGE0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("AxeGun/AXEGF0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("AxeGun/AXEGG0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("AxeGun/AXEGH0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("AxeGun/AXEGI0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("AxeGun/AXEGJ0", typeof(Texture)));
+		textures.Add((Texture)Resources.Load("AxeGun/AXEGK0", typeof(Texture)));
 		texture = textures[0];
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1") && Time.timeScale > 0) {
-			Debug.Log("Normal Gun Firing");
+			Debug.Log("Axe Gun Firing");
 			Attack();
 		}
 	}
-
+	
 	void OnGUI() {
-		Rect rect = new Rect(((Screen.width / 2) - (texture.width * 2)),
+		Rect rect = new Rect(((Screen.width / 2) - (texture.width)),
 		                     (Screen.height - (texture.height * 2)),
-		                     texture.width * 4,
+		                     texture.width * 2,
 		                     texture.height * 2);
 		GUI.DrawTexture(rect, texture);
 	}
-
+	
 	override public void Attack() {
 		GameObject bullet = (GameObject)Instantiate(Resources.Load("Bullet"));
 		var forward = Camera.main.transform.TransformDirection(Vector3.forward);
@@ -47,13 +53,13 @@ public class NormalGun : Weapon {
 		bullet.rigidbody.position = front;
 		bullet.rigidbody.velocity = Camera.main.transform.TransformDirection(velocityVector);
 		Debug.Log(bullet.rigidbody.velocity);
-
+		
 		Light lightGameObject = bullet.gameObject.AddComponent<Light> ();
 		lightGameObject.light.color = this.lightColor;
 		lightGameObject.light.intensity = this.lightIntensity;
-
+		
 		Destroy(bullet.gameObject, bulletLifeTime);
-
+		
 		StartCoroutine(PlayAnimation());
 	}
 	
--- emuhunter/Assets/Scripts/Weapons/Weapon.cs
+++ emuhunter/Assets/Scripts/Weapons/Weapon.cs
@@ -1,8 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public abstract class Weapon : MonoBehaviour {
 
+	protected Texture texture;
+	protected List<Texture> textures;
+	protected float fps = 20.0F;
+
 	public abstract void Attack();
+
+	protected IEnumerator PlayAnimation () {
+		float waitTime = 1.0F / fps;
+		for (int idx = 0; idx < textures.Count; ++idx) {
+			texture = textures[idx];
+			yield return new WaitForSeconds(waitTime);
+		}
+
+		texture = textures[0];
+	}
 
 }
