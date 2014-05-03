e9552cd6926a39dec67e1f78ddc3c8862a697c24
Christopher Horenstein <chris.horenstein@gmail.com>
2014-05-03 13:01:23 -0700

    Fixed gun not firing behind.

--- emuhunter/Assets/Scripts/Weapons/NormalGun.cs
+++ emuhunter/Assets/Scripts/Weapons/NormalGun.cs
@@ -4,22 +4,18 @@ using System.Collections;
 public class NormalGun : Weapon {
 	/// constants
 	public Vector3 velocityVector = new Vector3(0.0F, 0.0F, 100.0F);
-
 	private float bulletLifeTime = 3F;
 	private float lightIntensity = 10.0F;
 	private Color lightColor = Color.red + Color.yellow;
-	
-	private Transform ammoExit;
 
 	// Use this for initialization
 	void Start () {
-		ammoExit = this.transform;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1") && Time.timeScale > 0) {
-			Debug.Log("Normal Gun Firing.");
+			Debug.Log("Normal Gun Firing");
 			Attack();
 		}
 	}
@@ -27,9 +23,11 @@ public class NormalGun : Weapon {
 
 	override public void Attack() {
 		GameObject bullet = (GameObject)Instantiate(Resources.Load("Bullet"));
-		bullet.rigidbody.position = ammoExit.position;
-		bullet.rigidbody.rotation = Quaternion.identity;
-		bullet.rigidbody.velocity = transform.TransformDirection(velocityVector);
+		var forward = Camera.main.transform.TransformDirection(Vector3.forward);
+		var front = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 1.0f));
+		bullet.rigidbody.position = front;
+		bullet.rigidbody.velocity = Camera.main.transform.TransformDirection(velocityVector);
+		Debug.Log(bullet.rigidbody.velocity);
 
 		Light lightGameObject = bullet.gameObject.AddComponent<Light> ();
 		lightGameObject.light.color = this.lightColor;
