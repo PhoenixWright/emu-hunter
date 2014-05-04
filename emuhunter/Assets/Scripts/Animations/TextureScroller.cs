d433b6a9c9827ea3efaf3d86862b2dfde838e3fe
Christopher Horenstein <chris.horenstein@gmail.com>
2014-05-03 21:27:21 -0700

    Added texture scroller.

---
+++ emuhunter/Assets/Scripts/Animations/TextureScroller.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureScroller : MonoBehaviour {
+	public float scrollSpeed = 0.5F;
+	void Update() {
+		float offset = Time.time * scrollSpeed;
+		renderer.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+	}
+}
