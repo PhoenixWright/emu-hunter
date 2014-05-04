06e907564e47e159b1a3fccd704a758ae2bc2eb5
Christopher Horenstein <chris.horenstein@gmail.com>
2014-05-03 22:13:35 -0700

    Added ability to swap textures, added space skybox.

---
+++ emuhunter/Assets/Scripts/Animations/TextureManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureManager : MonoBehaviour {
+	static Texture texture;
+	public float scrollSpeed = 0.5F;
+
+	void Start() {
+		texture = Resources.Load<Texture>("dark_mixed_old_brick_texture_02");
+		renderer.material.SetTexture(0, texture);
+	}
+
+	void Update() {
+		float offset = Time.time * scrollSpeed;
+		renderer.material.SetTextureOffset("_MainTex", new Vector2(0, 0));//offset, 0));
+	}
+}
